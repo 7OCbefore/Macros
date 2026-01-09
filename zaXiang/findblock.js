a899fd3f6141cabce1eae5d3603555e2cdd83bbc
7OCbefore <7OCbefore@gmail.com>
2026-01-09 15:16:21 +0800

    feat: 删除农场自动化脚本文件
    删除了 shiLi/farm_area(by_Funzen).js 文件，该文件包含一个完整的
    Minecraft 农场自动化系统，包括作物种植、收割、重新种植和物品存储功能。
    此脚本还包含热键绑定、3D 渲染和复杂的物品管理逻辑。

--- zaXiang/findblock.js
+++
@@ -1,9 +0,0 @@
-var blist;
-var player = Player.getPlayer()
-blist = World.getWorldScanner().withStringBlockFilter().contains("minecraft:chest").build().scanAroundPlayer(16);
-Hud.clearDraw3Ds()
-for(let i = 0;i < blist.size();i++){
-    Hud.createDraw3D().register().addBox(blist[i].getX()+0.25, blist[i].getY()+0.25, blist[i].getZ()+0.25, blist[i].getX()+0.75, blist[i].getY()+0.75, blist[i].getZ()+0.75, 6749952, 0, true)
-    //Hud.createDraw3D().register().addTraceLine(blist[i].getX()+0.5, blist[i].getY()+0.5, blist[i].getZ()+0.5, 6749952)
-    //跟踪线
-    }
